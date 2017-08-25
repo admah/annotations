8b9c991cbc5dbecbae663ea61fad01f36e04abe2
Adam Murray - US <admurray@worldvision.org>
2017-08-25 16:53:45 -0500

    Start adding annotation display styling and functionality.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-	getContent(8);
+	init();
+	
+	function init() {
+		getContent(8);
+		getAnnotations(8);
+	}
 
 	// Function to request content via chapter number
 	function getContent(chapter) {
-
-		var contentChapter = chapter ? chapter.toString() : '';
-
-		if(contentChapter.length = 1) {
-			contentChapter = '0'+ contentChapter;
-		}
-
+		var contentChapter = sanitizeChapter(chapter);
 		var contentRequest = new Request(location.origin + '/annotations/assets/data/ch'+ contentChapter +'.txt', {
 			headers: new Headers({
 				'Content-Type': 'text/plain'
@@ -22,5 +21,45 @@ document.addEventListener('DOMContentLoaded', function(event) {
 			var contentContainer = document.querySelector('.contents');
 			contentContainer.innerHTML = text;
 		});
+	}
+
+	// Function to get initial annotation data
+	function getAnnotations(chapter) {
+		var contentChapter = sanitizeChapter(chapter);
+		var contentRequest = new Request(location.origin + '/annotations/assets/data/ch'+ contentChapter +'.txt.xml', {
+		});
+		var annotations;
+
+		fetch(contentRequest).then(function(response) {
+			return response.text();
+		}).then(function(text) {
+			var parser = new DOMParser();
+			var xmlDoc = parser.parseFromString(text, "text/xml");
+			var annotations = xmlDoc.getElementsByTagName('span');
+
+			displayAnnotations(annotations);
+		});
+	}
+
+	function sanitizeChapter(chapter) {
+		var contentChapter = chapter ? chapter.toString() : '';
+		
+		if(contentChapter.length = 1) {
+			contentChapter = '0'+ contentChapter;
+		}
+
+		return contentChapter;
+	}
+
+	function displayAnnotations(annotations) {
+		var contentContainer = document.querySelector('.contents').innerHTML;
+		var charSeq, span, category;
+
+		for(annotation of annotations){
+			category = annotation.getAttribute('category')
+			charSeq = annotation.getElementsByTagName('charseq');
+			charSeqLength = charSeq[0]['textContent'].length;
+			console.log(contentContainer.substr(charSeq[0].getAttribute('START'), charSeqLength), category);
+		};
 	}
 });
