6b6a016f859af501cfb2a4b932dc9fb7617cddea
Adam Murray - US <admurray@worldvision.org>
2017-08-25 14:28:52 -0500

    Add function to grab contents from initial chapter. Add annotation list and chapter list. Style content.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-	fetch('../data/ch08.txt').then(function(response) {
-		return response.blob();
-	}).then(function(myBlob) {
-		console.log(myBlob);
-	});
+	getContent(8);
+
+	// Function to request content via chapter number
+	function getContent(chapter) {
+
+		var contentChapter = chapter ? chapter.toString() : '';
+
+		if(contentChapter.length = 1) {
+			contentChapter = '0'+ contentChapter;
+		}
+
+		var contentRequest = new Request(location.origin + '/annotations/assets/data/ch'+ contentChapter +'.txt', {
+			headers: new Headers({
+				'Content-Type': 'text/plain'
+			})
+		});
+
+		fetch(contentRequest).then(function(response) {
+			return response.text();
+		}).then(function(text) {
+			var contentContainer = document.querySelector('.contents');
+			contentContainer.innerHTML = text;
+		});
+	}
 });
